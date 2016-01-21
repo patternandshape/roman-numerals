beb2a17e74a50310b01c5999561cb34e3b1cd19d
Abigail Rolling and Molly Curtin <patternandshape@gmail.com>
2016-01-21 15:42:33 -0800

    add working specs and scripts to project

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
 var breakDown = function(number) {
-  var romanThousands = [];
-  var romanFiveHundreds = [];
+  var romanBases = ["M", "D", "C", "L", "X", "V", "I"];
+  var arabicBases = [1000,500,100,50,10,5,1];
+  var romanNumeral = [];
 
-  if (number >= 1000) {
-    var thousands = Math.floor(number / 1000);
-    var remainder = number % 1000;
-    number = remainder;
-    for (var i=0; i < thousands; i++) {
-      romanThousands.push("M");
+
+  for (var i=0; i < romanBases.length ; i++) {
+    if (number >= arabicBases[i]) {
+      console.log(i);
+      //debugger;
+      var quantity = Math.floor(number / arabicBases[i]);
+      var remainder = number % arabicBases[i];
+      number = remainder;
+
+      for (var j = 0; j < quantity; j++) {
+        romanNumeral.push(romanBases[i]);
+        console.log(i, quantity, romanNumeral, number);
+      }
     }
-    return romanThousands;
   }
-
-  if (number >= 500) {
-    var fiveHundreds = Math.floor(number / 500);
-    var remainder = number % 500;
-    number = remainder;
-    for (var i=0; i < fiveHundreds; i++) {
-      romanFiveHundreds.push("D");
-    }
-    return romanFiveHundreds;
-  }
-
-
+  return romanNumeral;
 }
+  // if (number >= 1000) {
+  //   var thousands = Math.floor(number / 1000);
+  //   var remainder = number % 1000;
+  //   number = remainder;
+  //   for (var i=0; i < thousands; i++) {
+  //     romanThousands.push("M");
+  //   }
+  //   return romanThousands;
+  // }
+  //
+  // if (number >= 500) {
+  //   var fiveHundreds = Math.floor(number / 500);
+  //   var remainder = number % 500;
+  //   number = remainder;
+  //   for (var i=0; i < fiveHundreds; i++) {
+  //     romanFiveHundreds.push("D");
+  //   }
+  //   return romanFiveHundreds;
+  // }
--- spec/specs.js
+++ spec/specs.js
@@ -1,24 +1,24 @@
 describe ("breakDown", function(){
-  // it("Will convert ones to Roman bases", function() {
-  //   expect(breakDown(1)).to.eql(["I"]);
-  // });
-  //
-  // it("Will convert fives to Roman bases", function() {
-  //   expect(breakDown(5)).to.eql(["V"]);
-  // });
-  //
-  // it("Will convert tens to Roman bases", function() {
-  //   expect(breakDown(10)).to.eql(["C"]);
-  // });
-  //
-  // it("Will convert fifties to Roman bases", function() {
-  //   expect(breakDown(50)).to.eql(["L"]);
-  // });
-  //
-  // it("Will convert hundreds to Roman bases", function() {
-  //   expect(breakDown(100)).to.eql(["C"]);
-  // });
-  //
+  it("Will convert ones to Roman bases", function() {
+    expect(breakDown(1)).to.eql(["I"]);
+  });
+
+  it("Will convert fives to Roman bases", function() {
+    expect(breakDown(5)).to.eql(["V"]);
+  });
+
+  it("Will convert tens to Roman bases", function() {
+    expect(breakDown(10)).to.eql(["X"]);
+  });
+
+  it("Will convert fifties to Roman bases", function() {
+    expect(breakDown(50)).to.eql(["L"]);
+  });
+
+  it("Will convert hundreds to Roman bases", function() {
+    expect(breakDown(100)).to.eql(["C"]);
+  });
+
   it("Will convert five hundreds to Roman bases", function() {
     expect(breakDown(500)).to.eql(["D"]);
   });
